10b12291b1e3016c48845dc2d862c26cda1eb4fd
Roy <zhongroy189@gmail.com>
2022-06-01 10:41:09 -0700

    remove mui template

--- frontend/src/component/LoginPage.js
+++ frontend/src/component/LoginPage.js
@@ -1,10 +1,10 @@
 import React from "react";
-import SignIn from "./SignIn";
+import SignInPage from "./SignInPage";
 
 function Login() {
   return (
     <>
-      <SignIn />
+      <SignInPage />
     </>
   );
 }
