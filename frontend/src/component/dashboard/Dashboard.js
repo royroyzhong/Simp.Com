953ddea6f61acf1ef50aaae42de75a9f75e43c9f
Quinn <qintutao@gmail.com>
2022-06-03 18:15:24 -0700

    dev: [Quinn] impl + styled dashboard order status line

--- frontend/src/component/dashboard/Dashboard.js
+++ frontend/src/component/dashboard/Dashboard.js
@@ -1,16 +1,24 @@
 import { Card } from "@mui/material";
 import { Box, Container} from "@mui/system";
 import { Grid } from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import { getOrders } from "../../controller/userSlice";
+import "../../css/dashboard.css";
 
 export default function Dashboard(props) {
+
+    let orders = useSelector(getOrders);
 
     return (
         <Container maxWidth="bg">
             <Box sx={{ flexGrow: 1 }}>
-                <Grid spacing={{xs:2}} container columns={{ xs: 4, md: 4, bg: 12 }}>
-                    {Array.from(Array(4)).map((_, index) => (
-                    <Grid item xs={4} md={2} bg={3} key={index}>
-                        <OrderStatus />
+                <Box sx={{flexGrow: 1}}>
+                    <h3 id="orders-title">Orders</h3>
+                </Box>
+                <Grid spacing={{xs:2}} container columns={{ xs: 4, md: 4, lg: 12 }}>
+                    {Object.entries(orders).map(([key,val], index) => (
+                    <Grid item xs={4} md={2} lg={3} key={index}>
+                        <OrderStatus title={key} data={val}/>
                     </Grid>
                     ))}
                 </Grid>
@@ -22,9 +30,10 @@ export default function Dashboard(props) {
 function OrderStatus(props) {
 
     return (
-        <Card>
-            <div>
-                <p>hello world</p>
+        <Card variant="outlined">
+            <div className="card-wrapper">
+                <p className="card-title">{props.title}</p>
+                <p className="card-display">{props.data}</p>
             </div>
         </Card>
     )
