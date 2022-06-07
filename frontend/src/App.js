58bce06c43b48cb08fc4e3594c9a72f649d6adb0
Roy <zhongroy189@gmail.com>
2022-06-07 13:35:01 -0700

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <ProfilePage />
-      {/* <Dashboard /> */}
+      {/* <ProfilePage /> */}
+      <Dashboard />
     </div>
   );
 }
