0f65ce4ee1732b65cbfae3117219f41a7668128a
Roy <zhongroy189@gmail.com>
2022-06-07 13:29:03 -0700

    before merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Login from "./component/loginComponent/LoginPage";
-
+import ProfilePage from "./component/profileComponent/ProfilePage";
+// import ChangePassword from "./component/profileComponent/ChangePasswordPage";
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <ProfilePage />
     </div>
   );
 }
