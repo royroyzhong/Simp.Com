dd3b45298bedc44c498df50e336592737cb4cdd6
Quinn <qintutao@gmail.com>
2022-06-09 15:20:38 -0700

    dev: [Quinn] added customer page to commons

--- frontend/src/controller/store.js
+++ frontend/src/controller/store.js
@@ -1,18 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import CartSlice from "./CartSlice";
-=======
 import productSlice from "./productSlice";
->>>>>>> dev: [Quinn] added upload function
 import userSlice from "./userSlice";
 
 export default configureStore({
     reducer: {
         user: userSlice,
-<<<<<<< HEAD
-        cart: CartSlice
-=======
+        cart: CartSlice,
         products: productSlice
->>>>>>> dev: [Quinn] added upload function
     }
 })
