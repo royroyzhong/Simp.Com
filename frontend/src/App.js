e29b2c4e97abb658e39af1445971e8fd9c7436ce
Roy <zhongroy189@gmail.com>
2022-06-09 15:34:39 -0700

    before merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,38 @@
 // import logo from "./logo.svg";
-import OrderTracking from './component/orders/OrderTracking';
+import OrderTracking from "./component/orders/OrderTracking";
 import "./App.css";
 import Cart from "./component/cart/Cart";
 import Dashboard from "./component/dashboard/Dashboard";
 import Login from "./component/loginComponent/LoginPage";
-
-//import ProfilePage from "./component/profileComponent/ProfilePage";
-
-import ItemInCart from './component/cart/ItemInCart';
-
+import ProfilePage from "./component/profileComponent/ProfilePage";
+import ItemInCart from "./component/cart/ItemInCart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import CustomerPage from "./component/customerPageComponent/CustomerPage";
-
+import CustomerPage from "./component/customerPageComponent/CustomerPage";
 // import ChangePassword from "./component/profileComponent/ChangePasswordPage";
-
 
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Login />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <CustomerPage>
+                <ProfilePage />
+              </CustomerPage>
+            }
+          />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter> */}
-
+      </BrowserRouter>
 
       {/* <ProfilePage /> */}
       {/* <Dashboard /> */}
       {/* <Cart /> */}
-      {/*<OrderTracking /> */}
-      {/* <CustomerPage /> */}
-
+      {/* <OrderTracking /> */}
     </div>
   );
 }
--- frontend/src/component/customerPageComponent/CustomerPage.js
+++ frontend/src/component/customerPageComponent/CustomerPage.js
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Navigator from '../navigator/Navigator';
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Navigator from "../navigator/Navigator";
 import CustomerPageRigheContent from "./CustomerPageRightContent";
+import ProfilePage from "../profileComponent/ProfilePage";
 
 let theme = createTheme({});
 theme = {
@@ -11,15 +12,15 @@ theme = {
     MuiDrawer: {
       styleOverrides: {
         paper: {
-          backgroundColor: '#363740',
+          backgroundColor: "#363740",
         },
       },
     },
     MuiListItemButton: {
       styleOverrides: {
         root: {
-          '&.Mui-selected': {
-            color: '#4fc3f7',
+          "&.Mui-selected": {
+            color: "#4fc3f7",
           },
         },
       },
@@ -35,10 +36,10 @@ theme = {
     MuiListItemIcon: {
       styleOverrides: {
         root: {
-          color: 'inherit',
-          minWidth: 'auto',
+          color: "inherit",
+          minWidth: "auto",
           marginRight: theme.spacing(2),
-          '& svg': {
+          "& svg": {
             fontSize: 20,
           },
         },
@@ -49,24 +50,29 @@ theme = {
 
 const drawerWidth = 256;
 
-export default function Paperbase() {
-
+export default function Paperbase(props) {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: 'flex', minHeight: '200vh' }}>
+      <Box sx={{ display: "flex", minHeight: "200vh" }}>
         <Box
           component="nav"
           sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         >
           <Navigator
             PaperProps={{ style: { width: drawerWidth } }}
-            sx={{ display: { sm: 'block', xs: 'none' } }}
+            sx={{ display: { sm: "block", xs: "none" } }}
           />
         </Box>
-        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', bgcolor: '#F7F8FC'}}>
 
-            <CustomerPageRigheContent />
-          
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            bgcolor: "#F7F8FC",
+          }}
+        >
+          {props.children}
         </Box>
       </Box>
     </ThemeProvider>
