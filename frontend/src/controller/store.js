fade6d79ecd7796c2e6b674441d46f6b2ea7ba9e
IsabellaLCYang <64714074+IsabellaLCYang@users.noreply.github.com>
2022-06-10 21:28:10 -0700

    Path resolved

--- frontend/src/controller/store.js
+++ frontend/src/controller/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./cartSlice";
+import cartSlice from "./CartSlice";
 import productSlice from "./productSlice";
 import userSlice from "./userSlice";
 
