26d89c6ca752c90695ab8f0a07be086c8ec5c65b
Roy <zhongroy189@gmail.com>
2022-07-18 13:27:33 -0700

    add ui to display online/offline

--- server/routes/userProfile.js
+++ server/routes/userProfile.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
 const profileController = require("../controllers/profileController");
+const auth = require("../middleware/authJwt");
 
 router.put("/user/profile", profileController.updateProfile_put);
 router.put("/user/profile/password", profileController.updatePassword_put);
+router.get(
+  "/user/sellers",
+  auth.verifyToken,
+  profileController.getAllSeller_get
+);
 
 module.exports = router;
