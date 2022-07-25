fd63f55194e022adc9d36ff0ef2d97644bbf5cdf
Roy <zhongroy189@gmail.com>
2022-07-25 12:09:02 -0700

    fix live chat bugs

--- frontend/src/component/chat/SellerSearch.js
+++ frontend/src/component/chat/SellerSearch.js
@@ -1,6 +1,4 @@
 import React from "react";
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/Inbox";
 import { Box } from "@mui/system";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -13,34 +11,40 @@ import { useTheme } from "@mui/material/styles";
 import { TextField } from "@mui/material";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
+import MarkUnreadChatAltIcon from "@mui/icons-material/MarkUnreadChatAlt";
+import ChatIcon from "@mui/icons-material/Chat";
 const socket = io.connect("http://localhost:8888");
 function SellerSearch(prop) {
+  socket.emit("join_room", prop.self.email);
   const [open, setOpen] = React.useState(false);
 
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  // const theme = useTheme();
+  // const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
   const handleClose = () => {
     setOpen(false);
-    socket.disconnect();
+    // socket.disconnect();
   };
   const handleClickOpen = () => {
     setOpen(true);
-    socket.connect();
-    socket.emit("join_room", prop.self.email);
+    // socket.connect();
+    setMsgUnread(false);
   };
 
   const [message, setMessage] = useState("");
+  const [MsgUnread, setMsgUnread] = useState(false);
   const [messageReceived, setMessageReceived] = useState([]);
 
   useEffect(() => {
+    socket.connect();
     socket.on("receive_message", (data) => {
       let tempEle = {
         user: data.userName,
         message: data.message,
       };
       setMessageReceived((oldArray) => [...oldArray, tempEle]);
+      setMsgUnread(true);
     });
-  }, [socket]);
+  }, []);
 
   const handleSent = () => {
     if (message !== "") {
@@ -56,16 +60,26 @@ function SellerSearch(prop) {
       setMessageReceived((oldArray) => [...oldArray, tempEle]);
     }
   };
+  console.log(socket);
 
   return (
     <>
-      <InboxIcon
-        sx={{ fontSize: 39, color: "black", marginRight: "1vw" }}
-        onClick={handleClickOpen}
-      />
+      {!MsgUnread && (
+        <ChatIcon
+          sx={{ fontSize: 39, color: "black", marginRight: "1vw" }}
+          onClick={handleClickOpen}
+        />
+      )}
+      {MsgUnread && (
+        <MarkUnreadChatAltIcon
+          sx={{ fontSize: 39, color: "black", marginRight: "1vw" }}
+          onClick={handleClickOpen}
+        />
+      )}
+
       <Box>
         <Dialog
-          fullScreen={fullScreen}
+          // fullScreen={fullScreen}
           maxWidth="md"
           open={open}
           onClose={handleClose}
