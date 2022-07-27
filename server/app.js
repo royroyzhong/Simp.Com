8bdbfdd9b352fd4d0fe26ba4595040b534651118
Quinn <qintutao@gmail.com>
2022-07-27 01:05:50 -0700

    dev: [Quinn] adhoc fix added build script

--- server/app.js
+++ server/app.js
@@ -21,11 +21,11 @@ const myFormat = winston.format.printf(
   }
 );
 
-// Choose the port and start the server
-const PORT = process.env.PORT || 8888
-app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+// // Choose the port and start the server
+// const PORT = process.env.PORT || 8888
+// app.listen(PORT, () => {
+//   console.log(`Mixing it up on port ${PORT}`)
+// })
 
 app.use(
   expressWinston.logger({
