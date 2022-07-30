2fab0114b01e7310b2bcdfdcb0673add618ff513
IsabellaLCYang <64714074+IsabellaLCYang@users.noreply.github.com>
2022-07-30 00:07:35 -0700

    Support multiple files

--- server/frontend/src/component/common/DragDrop.js
+++ server/frontend/src/component/common/DragDrop.js
@@ -1,38 +1,26 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useCallback, useState } from "react";
+import { useDispatch} from "react-redux";
 import Dropzone from './Dropzone';
-import cuid from "cuid";
-import { Box, Container } from "@mui/system";
-import { getImages } from '../../controller/productSlice';
-import  {addImage} from "../../controller/productSlice";
+import { Box} from "@mui/system";
+import { addImage } from "../../controller/productSlice";
 
 export default function DragDrop() {
-    let dispatch = useDispatch();
-    
-    const onDrop = (file) => {
+  let dispatch = useDispatch();
+
+  const onDrop = (files) => {
+    files.map((file) => {
+      console.log(file);
       const reader = new FileReader();
       reader.onload = function (e) {
         dispatch(addImage(e.target.result));
       }
-      reader.readAsDataURL(file[0]);
-      return file[0];
-    }
-  // useCallback(acceptedFiles => {
-  //   acceptedFiles.map(file => {
-  //     const reader = new FileReader();
-  //     reader.onload = function(e) {
-  //       setImages(prevState => [
-  //         ...prevState,
-  //         { id: cuid(), src: e.target.result }
-  //       ]);
-  //     };
-  //     reader.readAsDataURL(file);
-  //     return file;
-  //   });
-  // }, []);
-      return (
-        <Box> 
-            <Dropzone onDrop = {onDrop} accept={"image/*"} > </Dropzone>
-        </Box>
-      )
+      reader.readAsDataURL(file);
+      return file;
+    })
+  }
+
+  return (
+    <Box>
+      <Dropzone onDrop={onDrop} accept={"image/*"} > </Dropzone>
+    </Box>
+  )
 }
