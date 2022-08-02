cc3e4bcba2fb337cd709e967fb2163f5b0ac4e88
Roy <zhongroy189@gmail.com>
2022-08-01 19:25:40 -0700

    before pull

--- server/frontend/src/component/common/NavWrapper.js
+++ server/frontend/src/component/common/NavWrapper.js
@@ -11,6 +11,9 @@ import Header from "./Header";
 import { Typography } from "@mui/material";
 import BuyerNavigationList from "../navigator/BuyerNavigationList";
 import SellerNavigationList from "../navigator/SellerNavigationList";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
 const drawerWidth = 240;
 
 const Drawer = styled(MuiDrawer, {
@@ -47,7 +50,13 @@ export default function NavWrapper(props) {
   const toggleDrawer = (childdata) => {
     setOpen(childdata);
   };
-
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  if (matches) {
+    if (open !== false) {
+      setOpen(false);
+    }
+  }
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: "flex" }}>
