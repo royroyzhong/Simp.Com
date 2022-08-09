48ac4dd8edda2aa07323de1f785e2b4f2f60ee79
Roy <zhongroy189@gmail.com>
2022-08-08 19:09:33 -0700

    profile

--- server/routes/userProfile.js
+++ server/routes/userProfile.js
@@ -4,7 +4,12 @@ const profileController = require("../controllers/profileController");
 const auth = require("../middleware/authJwt");
 
 router.put("/user/profile", profileController.updateProfile_put);
+router.put("/seller/user/profile", profileController.updateProfile_put);
 router.put("/user/profile/password", profileController.updatePassword_put);
+router.put(
+  "/seller/user/profile/password",
+  profileController.updatePassword_put
+);
 router.get(
   "/user/sellers",
   auth.verifyToken,
