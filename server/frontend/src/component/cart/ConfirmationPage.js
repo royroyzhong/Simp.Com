b481742a88d0fb354c5b6231906bb802e4cad544
IsabellaLCYang <64714074+IsabellaLCYang@users.noreply.github.com>
2022-08-10 02:08:12 -0700

    Fixed Responsiveness of cart wrapper

--- server/frontend/src/component/cart/ConfirmationPage.js
+++ server/frontend/src/component/cart/ConfirmationPage.js
@@ -1,5 +1,6 @@
 export default function ConfirmationPage() {
     return (
-        <h1> Your order has been recieved! More to come in the future </h1>
+        <h2> Your order has been recieved! 
+            More to come in the future </h2>
     )
 }
